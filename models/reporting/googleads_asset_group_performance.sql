c59dfc4187d7327670234cb17610656bc1699826
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-11-14 19:30:16 +0100

    Update googleads_asset_group_performance.sql

--- models/reporting/googleads_asset_group_performance.sql
+++ models/reporting/googleads_asset_group_performance.sql
@@ -8,6 +8,16 @@ campaign_name,
 campaign_id,
 campaign_status,
 campaign_type_default,
+case
+    when campaign_name ~* 'move' then 'MOVE'
+    when campaign_name ~* 'sweats' then 'Sweats'
+    when campaign_name ~* 'bfcm' then 'BFCM'
+    when campaign_name ~* 'tees' then 'Tees'
+    when campaign_name ~* 'tCPA' then 'NC tCPA'
+    when campaign_name ~* 'socks' then 'Socks'
+    when campaign_name ~* 'branded' then 'Branded'
+    else 'Other' 
+end as campaign_type_custom,
 asset_group_name,
 asset_group_id,
 asset_group_status,
