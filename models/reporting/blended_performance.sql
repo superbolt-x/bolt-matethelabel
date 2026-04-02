a0e31327ab3932664ea2c63d9532f2b2603355c5
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-04-02 17:34:24 +0200

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -40,27 +40,30 @@ WITH initial_sho_data AS
     sum(spend) as spend,
     sum(first_orders) as first_orders,
     sum(returning_orders) as returning_orders,
-    sum(subtotal_sales) as total_sales
+    sum(subtotal_sales) as total_sales,
+	sum(first_subtotal_sales) as first_total_sales
 from 
-(select date, date_granularity, 'Actual' as data_type, sum(spend) as spend, 0 as first_orders, 0 as returning_orders, 0 as subtotal_sales 
+(select date, date_granularity, 'Actual' as data_type, sum(spend) as spend, 0 as first_orders, 0 as returning_orders, 0 as subtotal_sales, 0 as first_subtotal_sales 
 from {{ source('reporting', 'facebook_ad_performance') }}
 where campaign_name != '[SB] Prospecting - Advantage+ - Video Testing'
 group by 1,2,3
 union all 
-select date, date_granularity, 'Actual' as data_type, sum(spend) as spend, 0 as first_orders, 0 as returning_orders, 0 as subtotal_sales 
+select date, date_granularity, 'Actual' as data_type, sum(spend) as spend, 0 as first_orders, 0 as returning_orders, 0 as subtotal_sales, 0 as first_subtotal_sales 
 from {{ source('reporting', 'googleads_campaign_performance') }}
 group by 1,2,3
 union all 
 select date, date_granularity, data_type, spend, shopify_first_orders as first_orders, 
     coalesce(shopify_orders,0) - coalesce(shopify_first_orders,0) as returning_orders, 
-    coalesce(shopify_sales,0) - coalesce(shopify_refund,0)  as subtotal_sales 
+    coalesce(shopify_sales,0) - coalesce(shopify_refund,0)  as subtotal_sales,
+	coalesce(shopify_first_sales,0) - coalesce(shopify_first_refund,0)  as first_subtotal_sales
 from sho_data)
 group by 1,2,3)
 
 , forcasted_data as (
 {%- for date_granularity in date_granularity_list %}
     select date_trunc('{{date_granularity}}',date)::date as date, '{{date_granularity}}' as date_granularity, 'Forecasted' as data_type, 
-    sum(original_spend) as original_spend, sum(original_spend) as spend, sum(acquisitions) as first_orders, sum(returning) as returning, sum(revenue) as total_sales
+    sum(original_spend) as original_spend, sum(original_spend) as spend, sum(acquisitions) as first_orders, sum(returning) as returning, sum(revenue) as total_sales,
+	sum(0) as first_total_sales
     from {{ source('gsheet_raw', 'forecast_data') }}
     group by 1,2,3
     {%- if not loop.last %}union all{%- endif %}
@@ -70,7 +73,8 @@ group by 1,2,3)
 , adjusted_forcasted_data as (
 {%- for date_granularity in date_granularity_list %}
     select date_trunc('{{date_granularity}}',date)::date as date, '{{date_granularity}}' as date_granularity, 'Adjusted Forecasted' as data_type, 
-    sum(adjusted_spend) as original_spend, sum(adjusted_spend) as spend, sum(adjusted_acquisitions) as first_orders, sum(adjusted_returning) as returning, sum(adjusted_revenue) as total_sales
+    sum(adjusted_spend) as original_spend, sum(adjusted_spend) as spend, sum(adjusted_acquisitions) as first_orders, sum(adjusted_returning) as returning, 
+	sum(adjusted_revenue) as total_sales, sum(0) as first_total_sales
     from {{ source('gsheet_raw', 'forecast_data') }}
     group by 1,2,3
     {%- if not loop.last %}union all{%- endif %}
